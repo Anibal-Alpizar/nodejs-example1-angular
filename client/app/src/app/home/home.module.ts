510ffe57ce17445fc711a75b373e453b0c836df6
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-06 15:33:46 -0600

    ng g component home/acerca-de --skip-tests

--- client/app/src/app/home/home.module.ts
+++ client/app/src/app/home/home.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { InicioComponent } from './inicio/inicio.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent,
+    AcercaDeComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
