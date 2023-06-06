ad637d136ae85bc51c58732f247547b8b2dcf5a7
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-06 16:39:49 -0600

    firsts pages

--- client/app/src/app/home/home-routing.module.ts
+++ client/app/src/app/home/home-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'acerca-de', component: AcercaDeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
--- client/app/src/app/home/home.module.ts
+++ client/app/src/app/home/home.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
 import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
-
 @NgModule({
-  declarations: [
-    InicioComponent,
-    AcercaDeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [InicioComponent, AcercaDeComponent],
+  imports: [CommonModule, HomeRoutingModule, MatCardModule],
 })
-export class HomeModule { }
+export class HomeModule {}
